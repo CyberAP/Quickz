182ce82adda9481a012e8e660c3cd80c6242d425
CyberAP <stasvarenkin@gmail.com>
2018-07-02 19:06:15 +0300

    * Rewrite event-based IO to Promise-based * Store last played game * Add CRUD for Game class

--- server/index.js
+++ server/index.js
@@ -1,71 +1,78 @@
 require('dotenv').config();
 import fs from 'fs';
+import path from 'path';
 import express from 'express';
 import http from 'http';
 import socket from 'socket.io';
 import history from 'connect-history-api-fallback';
+import { promisify } from 'util';
 import Game from './Game';
-import EventEmitter from 'events';
 
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 const port = process.env.VUE_APP_SOCKET_PORT || 3000;
 
-app.use(history());
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const unlink = promisify(fs.unlink);
+const stat = promisify(fs.stat);
 
-app.use(express.static(__dirname + '/dist'));
+const configPath = path.join(__dirname, 'server.config.json');
+let serverConfig;
+
+app.use(history());
+app.use(express.static(path.join(__dirname, '../', 'dist')));
 
 server.listen(port, function() {
     console.log(`Server is listening on: ${port}`);
 });
 
-class busEmitter extends EventEmitter {}
-const eventBus = new busEmitter;
-
-const savedGames = [];
+let savedGames;
+let currentGame;
 const gamesPath  = 'games';
-
-fs.readdir(gamesPath, (err, files) => {
-    files.forEach((file, key, arr) => {
-        fs.stat(`${gamesPath}/${file}`, (err, stats) => {
-            if (err || stats.isDirectory()) return;
-
-            fs.readFile(`${gamesPath}/${file}`, async (err, data) => {
-                if (err) return;
-                const savedGame = await JSON.parse(data);
-                const fileName = file.replace(/\.[^/.]+$/, '');
-                savedGames.push(Object.assign({ name: fileName }, savedGame));
-
-                if (key === arr.length -1) eventBus.emit('games_loaded');
-            });
-        });
-    });
-});
-
-let currentGame = new Game({
-    rounds: [
-        {
-            question: 'My question',
-            answer: 'Answer'
-        },
-        {
-            question: 'My question 2',
-            answer: 'Answer 2'
-        },
-    ]
-});
-
 const password = process.env.password || '123';
-
 const clients = new Set;
+
+const main = (async function() {
+    savedGames = (await loadSavedGames()).filter(game => game);
+    return loadConfig()
+        .catch(handleError)
+
+        .then(loadLastGame)
+        .catch(handleError);
+})();
 
 io.on('connection', socket => {
 
     let currentClient = '';
     let isAdmin = false;
 
+    const sync = () => {
+        return new Promise((resolve, reject) => {
+            if (!currentGame) reject('No game to sync');
+            const gameData = currentGame.publicData;
+            if (!gameData) reject('No game data available');
+            resolve(syncGameData(socket, gameData));
+        });
+    };
+
+    const syncAdmin = () => {
+        return new Promise((resolve, reject) => {
+            if (!isAdmin) reject('Not an admin');
+            resolve(syncAdminData(socket, { savedGames }));
+        });
+    };
+
     console.log('Client connected');
+
+    main
+        .then(sync)
+        .catch(handleError)
+
+        .then(syncAdmin)
+        .catch(handleError);
 
     socket.on('authorize', auth => {
         if (!auth || typeof auth !== 'string' || clients.has(auth)) return socket.emit('auth_fail');
@@ -76,7 +83,8 @@ io.on('connection', socket => {
 
         console.log(`Client ${currentClient} authorized`);
 
-        broadcast(socket, currentGame.publicData);
+        if (!currentGame) return;
+        sync().catch(handleError);
     });
 
     socket.on('log_out', logOut);
@@ -99,16 +107,18 @@ io.on('connection', socket => {
     ];
 
     commonActions.forEach(action => {
-        const actionCamelCase = action.replace(/_([a-z])/g, function (g) { return g[1].toUpperCase(); });
-        socket.on(action, () => {
-            currentGame[actionCamelCase]();
-            broadcast(socket, currentGame.publicData);
+        const actionCamelCase = action.replace(/_([a-z])/g, g => g[1].toUpperCase());
+        socket.on(action, data => {
+            if (!currentGame) return;
+            currentGame[actionCamelCase](data);
+            sync().catch(handleError);
         });
     });
 
     socket.on('attempt', () => {
+        if (!currentGame) return;
         currentGame.registerAttempt(currentClient);
-        broadcast(socket, currentGame.publicData);
+        sync().catch(handleError);
     });
 
 
@@ -117,23 +127,148 @@ io.on('connection', socket => {
         isAdmin = true;
         socket.emit('admin_success', adminPassword);
         console.log('Admin connected');
-        adminSync();
+        syncAdmin().catch(handleError);
     });
 
-    socket.on('score', (participant) => {
+    socket.on('score', participant => {
+        if (!currentGame || !isAdmin) return;
         currentGame.score(participant);
-        broadcast(socket, currentGame.publicData);
+        sync().catch(handleError);
     });
 
-    eventBus.on('games_loaded', adminSync);
-
-    function adminSync() {
+    socket.on('load_game', gameName => {
         if (!isAdmin) return;
-        socket.emit('admin_sync', {games: savedGames});
-    }
+        loadGame(gameName)
+            .catch(handleError)
+
+            .then(sync)
+            .catch(handleError);
+    });
+
+    socket.on('save_game', game => {
+        if (!isAdmin) return;
+        saveGame(game)
+            .catch(handleError)
+
+            .then(loadSavedGames)
+            .catch(handleError)
+
+            .then(syncAdmin)
+            .catch(handleError);
+    });
+
+    socket.on('delete_game', gameName => {
+        if (!isAdmin) return;
+        deleteGame(gameName)
+            .catch(handleError)
+
+            .then(loadSavedGames)
+            .catch(handleError)
+
+            .then(syncAdmin)
+            .catch(handleError);
+    });
 });
 
-function broadcast(socket, data) {
-    socket.emit('sync', data);
-    socket.broadcast.emit('sync', data);
+function loadSavedGames() {
+    return readDir(gamesPath)
+        .catch(handleError)
+        .then(files => {
+            const promises = files.map(file => {
+                const filePath = `${gamesPath}/${file}`;
+                return stat(filePath)
+                    .catch(handleError)
+                    .then(stats => {
+                        if (stats.isDirectory()) return;
+                        return readFile(filePath)
+                            .catch(handleError)
+                            .then(fileData => {
+                                const savedGame = JSON.parse(fileData);
+                                const fileName = file.replace(/\.[^/.]+$/, '');
+                                return Object.assign(savedGame, { name: fileName });
+                            });
+                    })
+                    .catch(handleError);
+            });
+            return Promise.all(promises);
+        });
+}
+
+function loadGame(gameName) {
+    return new Promise((resolve, reject) => {
+        if (!savedGames) return reject('Games are not ready yet');
+
+        const foundGame = savedGames.find(game => game.name === gameName);
+        if (!foundGame) return reject(`Game ${gameName} not found`);
+
+        currentGame = new Game(foundGame);
+        console.log(`Game ${currentGame.name} loaded`);
+        if (serverConfig && serverConfig.lastGameName !== gameName) saveConfig({ lastGameName: gameName });
+        resolve(currentGame);
+    });
+}
+
+function saveGame(game) {
+    const filename = `${gamesPath}/${game.name || new Date().toLocaleDateString("ru-RU")}.json`;
+
+    return writeFile(filename, JSON.stringify(game), 'utf8')
+        .catch(handleError)
+        .then(_ => game);
+}
+
+function deleteGame(gameName) {
+    const filename = `${gamesPath}/${gameName}.json`;
+
+    return unlink(filename);
+}
+
+function loadConfig() {
+    return readFile(configPath)
+        .catch(handleError)
+        .then(data => serverConfig = JSON.parse(data));
+}
+
+function saveConfig(newParams) {
+    const newConfig = Object.assign(serverConfig, newParams);
+
+    return writeFile(configPath, JSON.stringify(newConfig), 'utf8')
+        .catch(handleError)
+        .then(_ => serverConfig = newConfig);
+}
+
+function loadLastGame(config) {
+    const lastGameName = config.lastGameName;
+    return new Promise((resolve, reject) => {
+        if (lastGameName) {
+            if (savedGames.length) {
+                resolve(loadGame(lastGameName));
+            }
+        }
+        reject('No last game recorded');
+    });
+}
+
+function broadcast(socket, eventName, ...data) {
+    socket.emit(eventName, ...data);
+    socket.broadcast.emit(eventName, ...data);
+}
+
+function syncAdminData(socket, adminData) {
+    return new Promise((resolve, reject) => {
+        if (!adminData) reject('No admin data passed to sync');
+        socket.emit('admin_sync', adminData);
+        resolve(adminData);
+    });
+}
+
+function syncGameData(socket, gameData) {
+    return new Promise((resolve, reject) =>  {
+        if (!gameData) reject('No data for sync');
+        broadcast(socket, 'sync', gameData);
+        resolve(gameData);
+    });
+}
+
+function handleError(error) {
+    console.log(error);
 }
